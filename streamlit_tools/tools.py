0f9f9746007ab3df40f8d48f7bd9570dc5cb1653
huynh ba thien <neiths@users.noreply.huggingface.co>
2024-07-24 15:47:10 +0000

    Update streamlit_tools/tools.py

--- streamlit_tools/tools.py
+++ streamlit_tools/tools.py
@@ -15,7 +15,7 @@ from opentelemetry.sdk.trace.export import SimpleSpanProcessor
 #  Tracing LLM inference
 def setup_tracing_llm():
     px.launch_app()
-    endpoint = "http://127.0.0.1:6006/v1/traces"
+    endpoint = "http://localhost:6006/v1/traces"
     resource = Resource(attributes={})
     tracer_provider = trace_sdk.TracerProvider(resource=resource)
     span_otlp_exporter = OTLPSpanExporter(endpoint=endpoint)
